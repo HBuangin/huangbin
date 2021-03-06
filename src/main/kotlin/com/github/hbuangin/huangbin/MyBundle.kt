d94d682df6df46b9caf9b1311170a47b01314851
GitHub Action <action@github.com>
2021-03-06 03:30:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/hbuangin/huangbin/MyBundle.kt
+++ src/main/kotlin/com/github/hbuangin/huangbin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hbuangin.huangbin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hbuangin/huangbin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hbuangin/huangbin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hbuangin.huangbin.services
 
+import com.github.hbuangin.huangbin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
